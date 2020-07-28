34e0e0e796475f6fe4878f34d9e3f9afde0ed9a5
Varvara <Vintentas@gmail.com>
2020-07-28 14:51:37 +0300

    add new changes

--- src/Patterns/Structural/Composite.java
+++ src/Patterns/Structural/Composite.java
@@ -1,4 +1,76 @@
 package Patterns.Structural;
+/*
+Компоновщик — это структурный паттерн проектирования, который позволяет сгруппировать
+множество объектов в древовидную структуру, а затем работать с ней так, как будто это единичный объект.
+ */
+
+import java.util.ArrayList;
 
 public class Composite {
+    public static void main(String[] args) {
+        Thing many = new Something("some many");
+        Thing key = new Something("a key");
+        Thing dress = new Something("a dress");
+        Thing passport = new Something("a passport");
+        Thing shoes = new Something("a shoes");
+
+        Bag littleBag = new Bag("a littleBag");
+        littleBag.addThings(many);
+        littleBag.addThings(key);
+        littleBag.addThings(passport);
+
+        Bag bigBag = new Bag("a bigBag");
+        bigBag.addThings(dress);
+        bigBag.addThings(shoes);
+        bigBag.addThings(littleBag);
+
+        littleBag.removeThings(key);
+        bigBag.print();
+
+    }
+}
+
+interface Thing {
+    void print();
+
+}
+
+class Something implements Thing {
+    String name;
+
+    Something (String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("I'am " + name);
+    }
+}
+
+class Bag implements Thing {
+    ArrayList<Thing> things = new ArrayList();
+    String name;
+
+    Bag (String name) {
+        this.name = name;
+    }
+
+    public void addThings (Thing thing) {
+        things.add(thing);
+    }
+
+    public void removeThings (Thing thing) {
+        things.remove(thing);
+    }
+
+    @Override
+    public void print() {
+        if(things != null) {
+            System.out.println("I'am " + name + ". I have: ");
+            for (Thing thing : things) {
+                thing.print();
+            }
+        }
+    }
 }
--- src/Patterns/Structural/Decorator_OR_Wrapper.java
+++ src/Patterns/Structural/Decorator_OR_Wrapper.java
@@ -6,4 +6,72 @@ package Patterns.Structural;
  */
 
 public class Decorator_OR_Wrapper {
+    public static void main(String[] args) {
+        Printer printer =
+                new PrinterImpDecoratorAround(
+                        new PrinterImpDecoratorBefore(
+                                new PrinterImpDecoratorAfter(
+                                        new PrinterImp("world"))));
+        printer.print();
+    }
+}
+
+interface Printer {
+    void print();
+}
+
+class PrinterImp implements Printer {
+    String value;
+
+    PrinterImp(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public void print() {
+        System.out.print(value);
+    }
+}
+
+class PrinterImpDecoratorAfter implements Printer {
+    Printer printer;
+
+    PrinterImpDecoratorAfter (Printer printer) {
+        this.printer = printer;
+    }
+
+    @Override
+    public void print() {
+        printer.print();
+        System.out.print("!");
+    }
+}
+
+class PrinterImpDecoratorBefore implements Printer {
+    Printer printer;
+
+    PrinterImpDecoratorBefore (Printer printer) {
+        this.printer = printer;
+    }
+
+    @Override
+    public void print() {
+        System.out.print("Hello ");
+        printer.print();
+    }
+}
+
+class PrinterImpDecoratorAround implements Printer {
+    Printer printer;
+
+    PrinterImpDecoratorAround (Printer printer) {
+        this.printer = printer;
+    }
+
+    @Override
+    public void print() {
+        System.out.print(" ~~~ ");
+        printer.print();
+        System.out.print(" ~~~ ");
+    }
 }
